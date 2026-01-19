ecc885f4a5859effc207a6423381e29a42eb922b
felix <lilao@163.com>
2026-01-19 15:27:40 +0800

    test success in local

--- others/zy_6687/main.cpp
+++ others/zy_6687/main.cpp
@@ -3,44 +3,82 @@ using namespace std;
 vector<int> a;
 struct Node{
  vector<int> ans;
- long long score;   
+ vector<int> pos_list;
+ long long score=0;   
 };
+
+Node best_node;
 
 long long c, n;
 
+bool is_better(Node node_new){
+   if(best_node.score == 0){
+    return true;
+   }
+   int len = min(best_node.pos_list.size(), node_new.pos_list.size());
+   for(int i=0;i<len;i++){
+    if(best_node.pos_list[i] != node_new.pos_list[i]){
+        return node_new.pos_list[i]< best_node.pos_list[i] ;
+    }
+   }
+
+   if(node_new.pos_list.size() < best_node.pos_list.size()){
+    return true;
+   }else{
+    return false;
+   }
+}
+
 int dfs(int pos, Node node){
+    // cout<< endl <<"pos="<<pos<<" " <<endl;
+    // cout<<"best_node.scre 0: ";
+    // for(int i=0;i<best_node.pos_list.size();i++){
+    //     cout<<a[best_node.pos_list[i]]<<" ";
+    // }
+    // cout<<endl;
+    if(is_better(node)){
+
+        // return 0;
+    }else{
+        return 0;
+    }
+
     Node node_new = node;
     node_new.ans.push_back(a[pos]);
+    node_new.pos_list.push_back(pos);
     node_new.score += a[pos];
 
+    // cout<<"better  pos_list: ";
+    // for(int i=0;i<node.pos_list.size();i++){
+    //     cout<<a[node.pos_list[i]]<<" ";
+    // }
+    // cout<<endl;
+
     if(node_new.score == c){
-        for(int i=0;i<node_new.ans.size();i++){
-            cout<<node_new.ans[i]<<" ";
+        // for(int i=0;i<node_new.ans.size();i++){
+        //     cout<<node_new.ans[i]<<" ";
+        // }
+        if(is_better(node_new)){
+            best_node = node_new;
         }
-        return 1;
+        // return 1;
     }
 
-    if(node_new.score > c){
+    if(node_new.score < c){ //小于c，继续dfs
+        int flag_left = dfs(pos+1, node_new);
+    }
+
+    if(pos == a.size()-1){//dfs结束
         return 0;
     }
 
-    if(pos == a.size()-1){
-        return 0;
-    }
+    int flag_right = dfs(pos+1, node); //不选当前元素，继续dfs
 
-    int flag_left = dfs(pos+1, node_new);
-    if(flag_left == 1){
-        return 1;
-    }
-    int flag_right = dfs(pos+1, node);
-    if(flag_right == 1){
-        return 1;
-    }
     return 0;
 }
 
 int main(){
-    // freopen("input.in", "r", stdin);
+    freopen("input.in", "r", stdin);
     
     cin>>n>>c;
 
@@ -52,8 +90,13 @@ int main(){
     // sort(a.begin(), a.end());
 
     int flag =  dfs(0, Node());
-    if(flag == 0){
+    if(best_node.score == 0){
         cout<<"No solution!"<<endl;
+    }else{
+        for(int i=0;i<best_node.ans.size();i++){
+            cout<<a[best_node.pos_list[i]]<<" ";
+        }
+        cout<<endl;
     }
     return 0;
 }
